ce3774c18e4ed57943caefbe1732a3d9e5cf04c2
unknown <troysteedmiller@gmail.com>
2022-10-25 21:05:22 -0700

    added second API and works

--- assets/script.js
+++ assets/script.js
@@ -46,6 +46,7 @@ const firstUrl = "http-cors-proxy.p.rapidapi.com/"
 var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById('fetch-button');
 var requestUrl = 'https://generatorfun.com/consumeapi.php?api=39&apisecret=978c05-79094e-b07c06-d75f84-57ad6e'
+var requestUrltwo = 'https://generatorfun.com/consumeapi.php?api=2325&apisecret=944d63-8986a4-ed361f-a7c6a4-bf1f03'
 const fetchDataBtn = document.querySelector('#fetchdata')
 const result = document.querySelector('#result')
 
@@ -69,8 +70,34 @@ const getData = function() {
 }
 
 
+
 // add event listener for #fetchdata button
 fetchDataBtn.addEventListener('click', getData)
+
+
+
+const getData2 = function() {
+  result.innerText = 'Loading....'
+  const options = {
+    method: 'GET',
+    headers: {
+      origin: 'example.com',
+      'x-requested-with': 'example.com',
+      'X-RapidAPI-Key': '7a045ebe84mshf1fbfc93a63591fp1b38b4jsnf62ad6f91c8e',
+      'X-RapidAPI-Host': 'http-cors-proxy.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://http-cors-proxy.p.rapidapi.com/https://generatorfun.com/consumeapi.php?api=2325&apisecret=944d63-8986a4-ed361f-a7c6a4-bf1f03', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+
+
+
+// add event listener for #fetchdata button
+fetchDataBtn.addEventListener('click', getData2)
 
  
 
